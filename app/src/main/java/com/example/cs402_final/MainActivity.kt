8617aab945413e866a28581c3285795c3be41f1d
Landon <landoncurtis@u.boisestate.edu>
2021-12-03 16:07:56 -0700

    Added a button to the main activity. The button takes you to ScanActivity where a camera view and button are available. The api's are almost integrated to be able to identify barcodes but more testing is needed

--- app/src/main/java/com/example/cs402_final/MainActivity.kt
+++ app/src/main/java/com/example/cs402_final/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.cs402_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Code for Scanning the UPC Button
+        val butto = findViewById<Button>(R.id.ScanUPCButton)
+        butto.setOnClickListener{
+            val scanIntent = Intent(this,ScanActivity::class.java)
+            startActivity(scanIntent)
+        }
     }
+
+
+
 }
